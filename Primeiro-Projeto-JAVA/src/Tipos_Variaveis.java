60599a73a6f93b5c5ad8bfa8af1c7cce5d6197e3
LosSantins <vieirarg1@outlook.com>
2023-05-07 05:08:15 -0300

    operadores

--- Primeiro-Projeto-JAVA/src/Tipos_Variaveis.java
+++ Primeiro-Projeto-JAVA/src/Tipos_Variaveis.java
@@ -2,5 +2,16 @@ public class Tipos_Variaveis {
 
     public static void main(String[] args) {
             
+        int number = 5;
+
+        System.out.println(number);
+
+        number = -number;
+
+        System.out.println(number);
+
+        number = number * -1;
+
+        System.out.println(number);
     }
 }
