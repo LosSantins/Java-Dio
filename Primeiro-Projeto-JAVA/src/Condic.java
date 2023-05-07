baa1ade37c21b53d25ea9ef38c544f6daee9b046
LosSantins <vieirarg1@outlook.com>
2023-05-07 05:33:08 -0300

    if else

---
+++ Primeiro-Projeto-JAVA/src/Condic.java
@@ -0,0 +1,23 @@
+public class Condic {
+
+    public static void main(String[] args) {
+        
+        int a, b;
+
+        a = 11;
+        b = 11;
+
+        String status = " ";
+
+        if (a==b) {
+            status = "igual";
+        }
+
+        else
+            status = "diferente";
+
+        System.out.println(status);
+        
+    }
+    
+}
